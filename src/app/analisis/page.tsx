95e26cde54b93de1355f3a0d22c5029b72c00865
Fernando Vargas <fvargas1644@gmail.com>
2024-10-22 21:23:09 -0500

    Modificar consultas a la API de yfinance

--- src/app/analisis/page.tsx
+++ src/app/analisis/page.tsx
@@ -1,12 +1,20 @@
-import { getCookie } from "@/lib/yfinance-js/getData";
+import { getCookie, getCrumb, getParams } from "@/lib/yfinance-js/getData";
 
 export default async function AnalysisPage() {
-  const cookie = await getCookie()
+  const getcookie  = await getCookie()
+  if(getcookie.cookie){ 
+    const crumb = await getCrumb(getcookie.cookie)
+    if(crumb){
+      const params = await getParams(getcookie.cookie, "AAPL", getcookie.cookie)
+      console.log(params)
+    }
+  } 
+
+  console.log(getcookie.error)
 
     return (
       <div>
         <p>en page de analisis</p>
-        <p>{cookie}</p>
       </div>
     );
   }
--- src/lib/yfinance-js/getData.ts
+++ src/lib/yfinance-js/getData.ts
@@ -3,6 +3,38 @@
 const userAgent: string =
   "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36";
 
+export async function getCookie() {
+  try {
+    const response = await fetch("https://fc.yahoo.com", {
+      method: "GET",
+      headers: {
+        "User-Agent": userAgent,
+      },
+    });
+    const setCookie = response.headers.get("set-cookie");
+
+    if (setCookie) {
+      const cookie: string = setCookie.split(";")[0].trim();
+      return { 
+        cookie,
+        error: null
+      }
+    }
+
+    console.error("Error en decodificar cookie");
+    return { 
+      cookie: null,
+      error: 'Decoding error cookie'
+    };
+  } catch (error) {
+    console.error("Error fetching cookie:", error);
+    return { 
+      cookie: null,
+      error: 'Falló en fetching de cookie'
+    };
+  }
+}
+
 export async function getCrumb(cookie : string) {
   if (cookie) {
     try {
@@ -21,24 +53,29 @@ export async function getCrumb(cookie : string) {
       );
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error status: ${response.status}`);
       }
 
       const result: string = await response.text();
+
       return result;
     } catch (error) {
       return error instanceof Error ? error : new Error(String(error));
     }
   } else {
-    return null;
+    console.error('Cookie error: cookie is null');
   }
 }
 
-export async function getParams(cookie : string){
-  if (cookie) {
+export async function getParams(cookie : string, stock: string = 'AAPL', crumb: string){
+  const paramsArr = ['annualTotalRevenue', 'annualNetIncome', 'annualFreeCashFlow']
+  const paramsString = paramsArr.map(String).join(',');
+  const url = `https://query2.finance.yahoo.com/ws/fundamentals-timeseries/v1/finance/timeseries/${stock}?symbol=${stock}&type=${paramsString}&period1=1483142400&period2=1729555200&crumb=${crumb}`;
+  
+  if (cookie && crumb) {
     try {
       const response = await fetch(
-        "https://query2.finance.yahoo.com/ws/fundamentals-timeseries/v1/finance/timeseries/AAPL?symbol=AAPL&type=annualTaxEffectOfUnusualItems,annualTaxRateForCalcs,annualNormalizedEBITDA,annualNormalizedDilutedEPS,annualNormalizedBasicEPS,annualTotalUnusualItems,annualTotalUnusualItemsExcludingGoodwill,annualNetIncomeFromContinuingOperationNetMinorityInterest,annualReconciledDepreciation,annualReconciledCostOfRevenue,annualEBITDA,annualEBIT,annualNetInterestIncome,annualInterestExpense,annualInterestIncome,annualContinuingAndDiscontinuedDilutedEPS,annualContinuingAndDiscontinuedBasicEPS,annualNormalizedIncome,annualNetIncomeFromContinuingAndDiscontinuedOperation,annualTotalExpenses,annualRentExpenseSupplemental,annualReportedNormalizedDilutedEPS,annualReportedNormalizedBasicEPS,annualTotalOperatingIncomeAsReported,annualDividendPerShare,annualDilutedAverageShares,annualBasicAverageShares,annualDilutedEPS,annualDilutedEPSOtherGainsLosses,annualTaxLossCarryforwardDilutedEPS,annualDilutedAccountingChange,annualDilutedExtraordinary,annualDilutedDiscontinuousOperations,annualDilutedContinuousOperations,annualBasicEPS,annualBasicEPSOtherGainsLosses,annualTaxLossCarryforwardBasicEPS,annualBasicAccountingChange,annualBasicExtraordinary,annualBasicDiscontinuousOperations,annualBasicContinuousOperations,annualDilutedNIAvailtoComStockholders,annualAverageDilutionEarnings,annualNetIncomeCommonStockholders,annualOtherunderPreferredStockDividend,annualPreferredStockDividends,annualNetIncome,annualMinorityInterests,annualNetIncomeIncludingNoncontrollingInterests,annualNetIncomeFromTaxLossCarryforward,annualNetIncomeExtraordinary,annualNetIncomeDiscontinuousOperations,annualNetIncomeContinuousOperations,annualEarningsFromEquityInterestNetOfTax,annualTaxProvision,annualPretaxIncome,annualOtherIncomeExpense,annualOtherNonOperatingIncomeExpenses,annualSpecialIncomeCharges,annualGainOnSaleOfPPE,annualGainOnSaleOfBusiness,annualOtherSpecialCharges,annualWriteOff,annualImpairmentOfCapitalAssets,annualRestructuringAndMergernAcquisition,annualSecuritiesAmortization,annualEarningsFromEquityInterest,annualGainOnSaleOfSecurity,annualNetNonOperatingInterestIncomeExpense,annualTotalOtherFinanceCost,annualInterestExpenseNonOperating,annualInterestIncomeNonOperating,annualOperatingIncome,annualOperatingExpense,annualOtherOperatingExpenses,annualOtherTaxes,annualProvisionForDoubtfulAccounts,annualDepreciationAmortizationDepletionIncomeStatement,annualDepletionIncomeStatement,annualDepreciationAndAmortizationInIncomeStatement,annualAmortization,annualAmortizationOfIntangiblesIncomeStatement,annualDepreciationIncomeStatement,annualResearchAndDevelopment,annualSellingGeneralAndAdministration,annualSellingAndMarketingExpense,annualGeneralAndAdministrativeExpense,annualOtherGandA,annualInsuranceAndClaims,annualRentAndLandingFees,annualSalariesAndWages,annualGrossProfit,annualCostOfRevenue,annualTotalRevenue,annualExciseTaxes,annualOperatingRevenue,annualLossAdjustmentExpense,annualNetPolicyholderBenefitsAndClaims,annualPolicyholderBenefitsGross,annualPolicyholderBenefitsCeded,annualOccupancyAndEquipment,annualProfessionalExpenseAndContractServicesExpense,annualOtherNonInterestExpense&period1=1483142400&period2=1729555200&crumb=Ez2YEJjvRye",
+        url,
         {
           method: "GET",
           headers: {
@@ -52,40 +89,20 @@ export async function getParams(cookie : string){
       );
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        console.error(`HTTP error status: ${response.status}`);
+        throw new Error(`HTTP error status: ${response.status}`);
       }
 
       const result: string = await response.text();
-      const data = JSON.stringify(result)
-      //console.log(typeof data)
-      
+      const data = JSON.parse(result)
       return data;
     } catch (error) {
       return error instanceof Error ? error : new Error(String(error));
     }
   } else {
-    return null;
+    console.error('Cookie error: cookie is null');
+    return { error: 'No'}
   }
 }
 
-export async function getCookie() {
-  try {
-    const response = await fetch("https://fc.yahoo.com", {
-      method: "GET",
-      headers: {
-        "User-Agent": userAgent,
-      },
-    });
-    const setCookie = response.headers.get("set-cookie");
 
-    if (setCookie) {
-      const cookie: string = setCookie.split(";")[0].trim();
-      return cookie;
-    }
-
-    return null;
-  } catch (error) {
-    console.error("Error fetching cookie:", error);
-    return null;
-  }
-}
