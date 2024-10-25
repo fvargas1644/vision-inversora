7a1246eda4cda29567d9159354ca87030ef11808
Fernando Vargas <fvargas1644@gmail.com>
2024-10-24 19:33:49 -0500

    Reorganizar archivos de la carpeta lib y seperar función para obtener el WACC en un archivo individual

--- src/lib/yfinance-js/getData.ts
+++ src/lib/yfinance-js/getData.ts
@@ -67,56 +67,6 @@ export async function fetchYFinance({ cookie, stock = "AAPL", crumb, params }: F
     }
 }
 
-export async function getWacc(stock: string = "AAPL") {
-    try {
-        const response = await fetch(
-            `https://www.gurufocus.com/term/wacc/${stock}`
-        );
-
-        if (!response.ok) {
-            console.error(`HTTP error status: ${response.status}`);
-            return {
-                data: null,
-                error: `HTTP error status: ${response.status}`,
-            };
-        }
-
-        const body = await response.text();
-
-        // Crear un elemento temporal para analizar el HTML
-        const titleMatch = body.match(/<h1[^>]*>(.*?)<\/h1>/s);
-        if (titleMatch) {
-            const TitleContenido = titleMatch[1].trim(); // .trim() para eliminar espacios en blanco
-            const waccMatch = TitleContenido.match(/:(\S+)%/);
-            if (waccMatch) {
-                const wacc = parseFloat(waccMatch[1]);
-                return {
-                    wacc,
-                    error: null,
-                };
-            } else {
-                console.error("Error WACC: WACC no found");
-                return {
-                    wacc: null,
-                    error: "Error WACC: WACC no found",
-                };
-            }
-        } else {
-            console.error("Error Title WACC: WACC no found");
-            return {
-                wacc: null,
-                error: "Error Title WACC: WACC no found",
-            };
-        }
-    } catch (error) {
-        console.error(`Error: ${String(error)}`);
-        return {
-            wacc: null,
-            error: `Error: ${String(error)}`,
-        };
-    }
-}
-
 interface YFinanceQueryParams {
     query?: string,
     stock?: string,
