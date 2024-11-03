409078aad7c01a4df94f8181e32006ff6bb7dcbd
Fernando Vargas <fvargas1644@gmail.com>
2024-11-03 12:28:35 -0500

    Agregar tabla basica de datos

--- src/app/analisis/[stock]/page.tsx
+++ src/app/analisis/[stock]/page.tsx
@@ -9,13 +9,57 @@ export default async function StockAnalysisPage({ params }: { params: { stock: s
         stockPrice
     } = await getFinancialData(params.stock)
 
-    console.log(previousYearsData)
+    console.log(futureYearsData)
 
     return (
-        <Header
-            stockName={params.stock}
-            stockPrice={stockPrice}
-            stockintrinsicPrice={Math.floor(intrinsicPrice * 100)/100}
-        />
+        <>
+            <Header
+                stockName={params.stock}
+                stockPrice={stockPrice}
+                stockintrinsicPrice={Math.floor(intrinsicPrice * 100)/100}
+            />
+
+            <table>
+                <thead>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <th key={item.year}>{item.year}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <td key={item.year}>{item.data.annualNetIncome.toLocaleString()}</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <td key={item.year}>{item.data.annualFreeCashFlow.toLocaleString()}</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <td key={item.year}>{item.data.annualTotalRevenue.toLocaleString()}</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <td key={item.year}>{(item.data.growthRate * 100).toFixed(2)}%</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <td key={item.year}>{(item.data.margins * 100).toFixed(2)}%</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {previousYearsData.map((item) =>(
+                            <td key={item.year}>{item.data.freeCashFlowDividedNetIncome.toFixed(2)}</td>
+                        ))}
+                    </tr>
+                </tbody>
+            </table>
+        </>
     )
 }
