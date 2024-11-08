cf06a4c733945e71a0f02b7ee3be2986327e247a
Fernando Vargas <fvargas1644@gmail.com>
2024-11-07 21:13:11 -0500

    Modificar css barra de selección

--- src/components/analisis/SelectionBar.tsx
+++ src/components/analisis/SelectionBar.tsx
@@ -6,13 +6,15 @@ export default function SelectionBar() {
         <section className={styles.vi_section}>
             <div className={styles.vi_bar_container}>
                 <select name="analysis" id="analysis">
-                    <option value="discount-free-cash-flow">Discount Free Cash Flow</option>
+                    <option value="discounted-free-cash-flow">Discounted Free Cash Flow</option>
                     <option value="prueba">prueba</option>
                 </select>
                 <input type="text" />
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 icon">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
+                <button className={styles.vi_search_button}>    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`size-6 ${styles.vi_search_icon}`}>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                </button>
             </div>
         </section>
     )
