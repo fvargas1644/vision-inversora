e4b78335c5005f4e45a7edecfad115e159d9857b
Fernando Vargas <fvargas1644@gmail.com>
2024-11-20 20:04:22 -0500

    modificar de botón a link la sección de busqueda de la barra de selección

--- src/components/analisis/SelectionBar.tsx
+++ src/components/analisis/SelectionBar.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import styles from '@/styles/analisis/selectionBar.module.css'
+import Link from "next/link";
 
 export default function SelectionBar() {
 
@@ -6,23 +9,23 @@ export default function SelectionBar() {
         <section className={styles.vi_section}>
             <div className={styles.vi_bar_container}>
                 <div>
-                    
                     <select name="analysis" id="analysis">
                         <option value="discounted-free-cash-flow">Discounted Free Cash Flow</option>
                         <option value="prueba">prueba</option>
                     </select>
-
                 </div>
                 <div>
                     
                 <input type="text" />
                 </div>
                 <div>
-                <button className={styles.vi_search_button}>    
+                <Link
+                    href={'/'} 
+                    className={styles.vi_search_link}>    
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`size-6 ${styles.vi_search_icon}`}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
-                </button>
+                </Link>
                 </div>
             </div>
         </section>
