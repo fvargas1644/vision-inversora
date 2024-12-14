d169db8e1709543be84a453c40f25f3b1193b672
Fernando Vargas <fvargas1644@gmail.com>
2024-12-13 22:58:54 -0500

    Agregar custom hook useSearch y mover en el lógica de busqueda
    Modificar nombres de variables para ser más descriptivos

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,52 +1,21 @@
 'use client'
 
 import styles from '@/styles/search.module.css'
-import usePath from '@/hooks/usePath';
-import { useState } from 'react';
 import useSearch from "@/hooks/useSearch";
-import { useDebouncedCallback } from 'use-debounce';
-import { CompanyTicker } from "@/lib/sec-edgar/definitions";
-import { useRouter } from 'next/navigation';
+
 
 export default function Search() {
-    const router = useRouter();
-    const path = usePath();
-    const [inputValue, setInputValue] = useState(path.stock);
-    const [searchItem, setSearchItem] = useState<CompanyTicker[]>([]);
-    const [isOverItem, setIsOverItem] = useState(false);
-
-    const hideResults = () => {
-        setSearchItem([]); 
-        setIsOverItem(false);
-    }
-
-    const handleSearch = useDebouncedCallback(async (value) => {
-        const result: CompanyTicker[] = await useSearch(value);
-        if (value !== "" && value !== " ") { 
-            setSearchItem(result); 
-        } else { 
-            hideResults()
-        }
-    }, 300);
-
-    const handleOnChangeInput = async (value: string) => {
-        setInputValue(value);
-        handleSearch(value);
-    }
-
-    const handleOnClickSearchItem = (ticker: string) => {
-        setInputValue(ticker);
-        hideResults()
-        router.push(`/analisis/${ticker}`);
-    }
-
-    const HandleSearchRedirection = () => {
-        if(inputValue !== "" && inputValue !== " "){ 
-            hideResults();
-            if (searchItem.length > 0) setInputValue(searchItem[0][2]);
-            (searchItem.length > 0)  ? router.push(`/analisis/${searchItem[0][2]}`) : router.push(`/analisis/${inputValue}`); 
-        }
-    }
+    const {
+        inputValue, 
+        updateInputValueAndSearch, 
+        redirectToAnalysisPageBasedOnInputOrResult, 
+        searchResults, 
+        isOverItem,
+        selectSearchResultAndNavigate,
+        setIsOverItemToTrue,
+        setIsOverItemToFalse
+    } = useSearch();
+    
 
     return (
         <div className={styles.vi_nav_search_container}>
@@ -55,25 +24,25 @@ export default function Search() {
                     <input
                         type="text"
                         value={inputValue}
-                        onChange={(event) => handleOnChangeInput(event.target.value)}
-                        onKeyDown={(event) => {if (event.key === "Enter") HandleSearchRedirection()}}
+                        onChange={(event) => updateInputValueAndSearch(event.target.value)}
+                        onKeyDown={(event) => {if (event.key === "Enter") redirectToAnalysisPageBasedOnInputOrResult()}}
                     />
                     <button
                         className={styles.vi_nav_search_button}
-                        onClick={() => HandleSearchRedirection()}
+                        onClick={redirectToAnalysisPageBasedOnInputOrResult}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`size-6 ${styles.vi_nav_search_icon}`}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
                     </button>
                 </div>
-                <div className={`${styles.vi_nav_search_results_container} ${searchItem.length === 0 ? styles.isHidden : ''}`}>
-                    {searchItem.map((item, index) => (
+                <div className={`${styles.vi_nav_search_results_container} ${searchResults.length === 0 ? styles.isHidden : ''}`}>
+                    {searchResults.map((item, index) => (
                         <div
                             className={`${styles.vi_nav_search_result_item} ${index === 0 && !isOverItem ? styles.vi_nav_first_search_result_item : ''}`}
-                            onClick={() => handleOnClickSearchItem(item[2])}
-                            onMouseOver={() => setIsOverItem(true)}
-                            onMouseOut={() => setIsOverItem(false)}
+                            onClick={() => selectSearchResultAndNavigate(item[2])}
+                            onMouseOver={setIsOverItemToTrue}
+                            onMouseOut={setIsOverItemToFalse}
                         >
                             <p>{item[1]}</p>
                             <p><strong>{item[2]}</strong></p>
--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,18 +1,77 @@
+'use client'
 import { fetchCompanyTickersExchange } from "@/lib/sec-edgar/fetchData";
 import { CompanyTicker, FetchCompanyTickersExchangeResponse } from "@/lib/sec-edgar/definitions";
+import { useState } from "react";
+import { useDebouncedCallback } from 'use-debounce';
+import { useRouter } from 'next/navigation';
+import usePath from '@/hooks/usePath';
 
-export default async function useSearch(stock: string): Promise<CompanyTicker[]> {
-    const fetch: FetchCompanyTickersExchangeResponse = await fetchCompanyTickersExchange()
+export default function useSearch(){
 
-    let resultArr = [];
-    for (let arr of fetch.data) {
-        if (arr[2].toLowerCase().includes(stock.toLowerCase()) || arr[1].toLowerCase().includes(stock.toLowerCase())) {
-            resultArr.push(arr)
+    const router = useRouter();
+    const path = usePath();
+    const [inputValue, setInputValue] = useState(path.stock);
+    const [searchResults, setSearchResults] = useState<CompanyTicker[]>([]);
+    const [isOverItem, setIsOverItem] = useState(false);
+
+    const fetchCompanyExchangeResults = async (stock : string) => {
+        const searchResults : CompanyTicker[]= [];
+        const fetchCompany: FetchCompanyTickersExchangeResponse = await fetchCompanyTickersExchange()
+
+        for (let item of fetchCompany.data) {
+            if (item[2].toLowerCase().includes(stock.toLowerCase()) || item[1].toLowerCase().includes(stock.toLowerCase())) {
+                searchResults.push(item)
+            }
+            if (searchResults.length > 9) break
         }
+        return searchResults;
+    }
 
-        if (resultArr.length > 9) break
+    const clearSearchResults = () => {
+        setSearchResults([]); 
+        setIsOverItem(false);
+    }
+
+    const setIsOverItemToTrue  = () => setIsOverItem(true)
+    const setIsOverItemToFalse  = () => setIsOverItem(false)
+
+    const performSearchWithDebounce = useDebouncedCallback(async (value) => {
+        const results: CompanyTicker[] = await fetchCompanyExchangeResults(value);
+        if (value !== "" && value !== " ") { 
+            setSearchResults(results); 
+        } else { 
+            clearSearchResults()
+        }
+    }, 300);
+
+    const updateInputValueAndSearch = async (value: string) => {
+        setInputValue(value);
+        performSearchWithDebounce(value);
+    }
+
+    const selectSearchResultAndNavigate = (ticker: string) => {
+        setInputValue(ticker);
+        clearSearchResults()
+        router.push(`/analisis/${ticker}`);
+    }
+
+    const redirectToAnalysisPageBasedOnInputOrResult = () => {
+        if(inputValue !== "" && inputValue !== " "){ 
+            clearSearchResults();
+            if (searchResults.length > 0) setInputValue(searchResults[0][2]);
+            (searchResults.length > 0)  ? router.push(`/analisis/${searchResults[0][2]}`) : router.push(`/analisis/${inputValue}`); 
+        }
     }
     
-    return resultArr;
+    return {
+        inputValue, 
+        updateInputValueAndSearch, 
+        redirectToAnalysisPageBasedOnInputOrResult, 
+        searchResults, 
+        isOverItem,
+        selectSearchResultAndNavigate,
+        setIsOverItemToTrue,
+        setIsOverItemToFalse
+    };
     
 }
