5c1b8df3369daf437cdbae93396897edae8b4934
Fernando Vargas <fvargas1644@gmail.com>
2024-12-17 12:39:44 -0500

    Refactorizar useState de el hook de useFormPreviousYears

--- src/hooks/analisis/discounted-free-cash-flow/useFormPreviousYears.ts
+++ src/hooks/analisis/discounted-free-cash-flow/useFormPreviousYears.ts
@@ -1,20 +1,40 @@
-import { validateWacc } from "@/lib/validation/discounted-free-cash-flow/validations"
+import { validateGrowth, validateWacc } from "@/lib/validation/discounted-free-cash-flow/validations"
 import { useState } from "react"
 
 function returnFormattedValueToPercent (value : number) {
     return Number((value*100).toFixed(2))
 }
 
+export interface FormData {
+    waccError: null | string,
+    growthError: null | string,
+    wacc: number,
+    growth: number
+}
+  
+
 export default function useFormPreviousYears ({wacc, growth} : {wacc: number, growth:number}) {
-    const [formData, setFormData] = useState({wacc: returnFormattedValueToPercent(wacc), growth : returnFormattedValueToPercent(growth)})
+    const [formData, setFormData] = useState<FormData>({
+        wacc: returnFormattedValueToPercent(wacc), 
+        growth: returnFormattedValueToPercent(growth),
+        waccError: null,
+        growthError: null
+    })
 
     const updateWaccInputValue = (value : string) => {
-        const valida = validateWacc(Number(value))
-        setFormData({...formData, wacc: Number(value)})
+        setFormData({
+            ...formData, 
+            wacc: Number(value),
+            waccError: validateWacc(Number(value))
+        })
     }
 
     const updateGrowthInputValue = (value: string) => {
-        setFormData({...formData, growth: Number(value)})
+        setFormData({
+            ...formData, 
+            growth: Number(value),
+            growthError: validateGrowth(Number(value))
+        })
     }
 
     const sendData = () => {
