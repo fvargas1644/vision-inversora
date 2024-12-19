4d02528dd7e6b8b70739ccb25a92ace34f56b1dc
Fernando Vargas <fvargas1644@gmail.com>
2024-12-19 09:50:48 -0500

    Renombrar clases css del formulario de años previos

--- src/components/analisis/FormPreviousYears.tsx
+++ src/components/analisis/FormPreviousYears.tsx
@@ -13,26 +13,30 @@ export default function FormPreviousYears({wacc, growth} : {wacc: number, growth
     } = useFormPreviousYears({wacc, growth})
 
     return (
-        <form className={styles.previusYears_form} action={sendData}> 
-            <label htmlFor="wacc">WACC</label>
-            <input 
-                type="number" 
-                className={`${formData.waccError && styles.hasWarning}`}
-                value={formData.wacc} 
-                onChange={(event) => updateWaccInputValue(event.target.value)} 
-                placeholder="WACC" 
-                id="wacc" 
-            />
-            <label htmlFor="crecimiento">Crecimiento</label>
-            <input 
-                type="number" 
-                value={formData.growth} 
-                className={`${formData.growthError && styles.hasWarning}`}
-                onChange={(event) => updateGrowthInputValue(event.target.value)} 
-                placeholder="Crecimiento" 
-                id="crecimiento" 
-            />
-            <button type="submit">Actualizar</button>
+        <form className={styles.vi_previusyears_form} action={sendData}> 
+                <label htmlFor="wacc">WACC</label>
+                <div className={styles.vi_previusyears_form_input_container}>
+                    <input 
+                        type="number" 
+                        className={`${styles.vi_previusyears_form_input} ${formData.waccError && styles.hasWarning}`}
+                        value={formData.wacc} 
+                        onChange={(event) => updateWaccInputValue(event.target.value)} 
+                        placeholder="WACC" 
+                        id="wacc" 
+                    />
+                    
+                </div>
+                <label htmlFor="crecimiento">Crecimiento</label>
+                <div className={styles.vi_previusyears_form_input_container}>
+                    <input 
+                        type="number" 
+                        value={formData.growth} 
+                        className={`${styles.vi_previusyears_form_input} ${formData.growthError && styles.hasWarning}`}
+                        onChange={(event) => updateGrowthInputValue(event.target.value)} 
+                        placeholder="Crecimiento" 
+                        id="crecimiento" 
+                    />
+                </div>
         </form>
     )
 }
