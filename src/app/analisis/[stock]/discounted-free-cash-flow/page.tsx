94cf4e1420fa38e1b0f73fa0d047f2e60dccd473
Fernando Vargas <fvargas1644@gmail.com>
2024-12-26 23:14:48 -0500

    Agregar componente en la página de disconted free cash flow el cual gestiona el todo el contenido de página en lado del cliente

--- src/app/analisis/[stock]/discounted-free-cash-flow/page.tsx
+++ src/app/analisis/[stock]/discounted-free-cash-flow/page.tsx
@@ -1,47 +1,14 @@
-import Description from "@/components/analisis/Description";
-import { Header } from "@/components/analisis/disconted-free-cash-flow/Header";
-import TableFutureData from "@/components/analisis/disconted-free-cash-flow/TableFutureData";
-import TablePreviousData from "@/components/analisis/disconted-free-cash-flow/TablePreviousData";
+
 import getFinancialData from "@/lib/discounted-free-cash-flow/getData";
-import styles from '@/styles/analisis/discounted-free-cash-flow/page.module.css'
-import FormPreviousYears from '@/components/analisis/disconted-free-cash-flow/FormPreviousYears'
 import DiscontedFreeCashFlowProvider from "@/context/DiscountedFreeCashFlowContext";
+import PageContent from "@/components/analisis/disconted-free-cash-flow/PageContent";
 
 export default async function StockAnalysisPage({ params }: { params: { stock: string } }) {
     const data = await getFinancialData(params.stock);
 
     return (
         <DiscontedFreeCashFlowProvider initialData={data}>
-            <section className="center_content">
-                <Header
-                    stockName={params.stock}
-                    stockPrice={data.stockPrice}
-                    stockintrinsicPrice={Math.floor(data.intrinsicPrice * 100) / 100}
-                >
-                    Disconted Free<br />Cash Flow
-                </Header>
-            </section>
-            <section className="center_content" style={{ backgroundColor: '#88D5BA' }}>
-                <Description title="">
-                    El método de flujo de caja descontado (DCF) con flujos de caja libres (FCF) es una técnica de
-                    valoración financiera que estima el valor presente de una empresa o activo basado en los flujos
-                    de caja futuros proyectados, descontados a una tasa que refleja el riesgo y el costo del capital (WACC).
-                    Se proyectan los flujos de caja libres para los próximos años, se calcula un valor terminal para el
-                    período posterior, y luego se descuentan todos estos valores al presente para obtener la valoración
-                    de la empresa. Este método es útil para evaluar la capacidad de una empresa para generar valor a
-                    largo plazo, pero depende de la precisión de las proyecciones de flujos y de una correcta selección
-                    de la tasa de descuento.
-                </Description>
-            </section>
-            <section className="center_content">
-                <div className="vi_page_container" style={{ padding: '20px' }}>
-                    <div className={styles.previusYears_container}>
-                        <TablePreviousData previousYearsData={data.previousYearsData} />
-                        <FormPreviousYears wacc={data.wacc} growth={data.growth} />
-                    </div>
-                    <TableFutureData futureYearsData={data.futureYearsData} />
-                </div>
-            </section>
+            <PageContent stock={params.stock}/>
         </DiscontedFreeCashFlowProvider>
     )
 }
---
+++ src/components/analisis/disconted-free-cash-flow/PageContent.tsx
@@ -0,0 +1,52 @@
+
+'use client'
+
+import styles from '@/styles/analisis/discounted-free-cash-flow/page.module.css'
+import FormPreviousYears from '@/components/analisis/disconted-free-cash-flow/FormPreviousYears'
+import Description from "@/components/analisis/Description";
+import { Header } from "@/components/analisis/disconted-free-cash-flow/Header";
+import TableFutureData from "@/components/analisis/disconted-free-cash-flow/TableFutureData";
+import TablePreviousData from "@/components/analisis/disconted-free-cash-flow/TablePreviousData";
+import { useContext } from 'react';
+import { DiscontedFreeCashFlowProviderContext } from '@/context/DiscountedFreeCashFlowContext';
+
+
+export default function PageContent({stock} : {stock : string}) {
+
+    const { financialData } = useContext(DiscontedFreeCashFlowProviderContext)
+
+    return (
+        <>
+            <section className="center_content">
+                <Header
+                    stockName={stock}
+                    stockPrice={financialData.stockPrice}
+                    stockintrinsicPrice={Math.floor(financialData.intrinsicPrice * 100) / 100}
+                >
+                    Disconted Free<br />Cash Flow
+                </Header>
+            </section>
+            <section className="center_content" style={{ backgroundColor: '#88D5BA' }}>
+                <Description title="">
+                    El método de flujo de caja descontado (DCF) con flujos de caja libres (FCF) es una técnica de
+                    valoración financiera que estima el valor presente de una empresa o activo basado en los flujos
+                    de caja futuros proyectados, descontados a una tasa que refleja el riesgo y el costo del capital (WACC).
+                    Se proyectan los flujos de caja libres para los próximos años, se calcula un valor terminal para el
+                    período posterior, y luego se descuentan todos estos valores al presente para obtener la valoración
+                    de la empresa. Este método es útil para evaluar la capacidad de una empresa para generar valor a
+                    largo plazo, pero depende de la precisión de las proyecciones de flujos y de una correcta selección
+                    de la tasa de descuento.
+                </Description>
+            </section>
+            <section className="center_content">
+                <div className="vi_page_container" style={{ padding: '20px' }}>
+                    <div className={styles.previusYears_container}>
+                        <TablePreviousData previousYearsData={financialData.previousYearsData} />
+                        <FormPreviousYears wacc={financialData.wacc} growth={financialData.growth} />
+                    </div>
+                    <TableFutureData futureYearsData={financialData.futureYearsData} />
+                </div>
+            </section>
+        </>
+    )
+}
