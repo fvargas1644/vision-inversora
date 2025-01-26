1d4ef1e1196cb7823fdae3bac32450e920e55d6c
Fernando Vargas <fvargas1644@gmail.com>
2025-01-25 19:24:46 -0500

    Modificar vizualización de valores de FinancialData

--- src/components/analisis/disconted-free-cash-flow/FinancialData.tsx
+++ src/components/analisis/disconted-free-cash-flow/FinancialData.tsx
@@ -1,6 +1,18 @@
 import styles from '@/styles/analisis/discounted-free-cash-flow/FinancialData.module.css'
 import { PreviousYearsDataType } from "@/lib/discounted-free-cash-flow/definitions"
 
+function formatPrice(price: number): string {
+    if (price >= 1_000_000_000) { 
+        return (price / 1_000_000_000).toFixed(3) + 'B';
+    } else if (price >= 1_000_000) { 
+        return (price / 1_000_000).toFixed(3) + 'M'; 
+    } else if (price >= 1_000) { 
+        return (price / 1_000).toFixed(3) + 'K'; 
+    } else {
+        return price.toString();
+    }
+}
+
 export default function FinancialData({ previousYearsData }: { previousYearsData: PreviousYearsDataType[] }) {
     return (
         <section className={styles.financialData}>
@@ -11,45 +23,26 @@ export default function FinancialData({ previousYearsData }: { previousYearsData
                 <tr>
                   
                   <th>Year</th>
-                  <th>Total Revenue</th>
-                  <th>Growth Rate</th>
-                  <th>Net Income</th>
+                  <th>Revenue</th>
+                  <th>GrowthRate</th>
+                  <th>NetIncome</th>
                   <th>Margins</th>
-                  <th>Free Cash Flow</th>
-                  <th>FCF Ratio</th>
+                  <th>FreeCashFlow</th>
+                  <th>FCFRatio</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>2023</td>
-                  <td>$22680.00M</td>
-                  <td className={styles.negative}>-3.9%</td>
-                  <td>$854.00M</td>
-                  <td>3.77%</td>
-                  <td>$1121.00M</td>
-                  <td>1.31</td>
-                </tr>
-                <tr>
-                  <td>2022</td>
-                  <td>$23601.00M</td>
-                  <td className={styles.positive}>43.61%</td>
-                  <td>$1320.00M</td>
-                  <td>5.59%</td>
-                  <td>$3115.00M</td>
-                  <td>2.36</td>
-                </tr>
                 {previousYearsData.map((item) => (
                     <tr key={item.year}>
                         <td>{item.year}</td>
-                        <td>{item.data.annualTotalRevenue.toLocaleString()}</td>
-                        <td>{(item.data.growthRate * 100).toFixed(2)}%</td>
-                        <td>{item.data.annualNetIncome.toLocaleString()}</td>
-                        <td>{(item.data.margins * 100).toFixed(2)}%</td>
-                        <td>{item.data.annualFreeCashFlow.toLocaleString()}</td>
+                        <td>${formatPrice(item.data.annualTotalRevenue)}</td>
+                        <td className={item.data.growthRate > 0 ? styles.positive : styles.negative}>{(item.data.growthRate * 100).toFixed(2)}%</td>
+                        <td>${formatPrice(item.data.annualNetIncome)}</td>
+                        <td className={item.data.growthRate > 0 ? styles.positive : styles.negative}>{(item.data.margins * 100).toFixed(2)}%</td>
+                        <td>${formatPrice(item.data.annualFreeCashFlow)}</td>
                         <td>{item.data.freeCashFlowDividedNetIncome.toFixed(2)}</td>
                     </tr>
                 ))}
-                {/* Add more rows as needed */}
               </tbody>
             </table>
           </div>
