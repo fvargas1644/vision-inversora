3197eebbcd11e48e651561c07703a505dc720f5c
Fernando Vargas <fvargas1644@gmail.com>
2025-03-23 12:40:12 -0500

    Cambiar configuración de next.config.js para permitir errores de tipado controlado

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,  // Ignora los errores de tipo (No esta por defecto) 
+      },
+};
 
 export default nextConfig;
